da1fffd9aec05cbe77457dfb9d0a808416ff1362
mirkobrombin <send@mirko.pm>
2020-01-30 15:14:05 +0100

    Login with GitHub button

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -3,26 +3,44 @@
     <div class="uk-container">
       <h3>Log in to your account</h3>
       <p>Use data entered during registration.</p>
-      <form>
-        <div class="uk-margin">
-          <div class="uk-inline">
-            <span class="uk-form-icon" uk-icon="icon: mail"></span>
-            <input class="uk-input" type="text" placeholder="E-mail">
+
+      <div class="uk-child-width-expand@s uk-text-center" uk-grid>
+        <div>
+          <form>
+            <div class="uk-margin">
+              <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: mail"></span>
+                <input class="uk-input" type="text" placeholder="E-mail">
+              </div>
+            </div>
+            <div class="uk-margin">
+              <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                <input class="uk-input" type="password" placeholder="Password">
+              </div>
+            </div>
+            <div class="uk-margin">
+              <label><input class="uk-checkbox" type="checkbox" checked> Remember me</label>
+            </div>
+            <div class="uk-margin">
+              <button class="uk-button uk-button-primary">Sign in</button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">
+            <h3 class="uk-card-title">Sign In with Social</h3>
+            <div class="uk-margin">
+              <button class="uk-button uk-button-secondary" uk-icon="github">GitHub</button>
+            </div>
           </div>
         </div>
-        <div class="uk-margin">
-          <div class="uk-inline">
-            <span class="uk-form-icon" uk-icon="icon: lock"></span>
-            <input class="uk-input" type="password" placeholder="Password">
-          </div>
+        <div>
+          <div class="uk-card uk-card-default uk-card-body">Item</div>
         </div>
-        <div class="uk-margin">
-          <label><input class="uk-checkbox" type="checkbox" checked> Remember me</label>
-        </div>
-        <div class="uk-margin">
-          <button class="uk-button uk-button-primary">Sign in</button>
-        </div>
-      </form>
+      </div>
+
+
     </div>
   </div>
 </template>
